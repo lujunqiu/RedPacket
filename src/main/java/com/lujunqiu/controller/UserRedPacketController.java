21c78428fd8812939f91b12d8c9c616dbfdb2c44
lujunqiu <2672543426@qq.com>
2018-01-09 14:55:12 +0800

    完成无锁环境下的测试，仅有数据库的事务支持，事务隔离级别READ_COMMITTED已提交读

---
+++ src/main/java/com/lujunqiu/controller/UserRedPacketController.java
@@ -0,0 +1,33 @@
+package com.lujunqiu.controller;
+
+import com.lujunqiu.service.UserRedPacketService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ *
+ * @author qiu
+ */
+@Controller
+@RequestMapping("/userRedPacket")
+public class UserRedPacketController {
+
+    @Autowired
+    private UserRedPacketService userRedPacketService = null;
+
+    @RequestMapping(value = "/grapRedPacket")
+    @ResponseBody
+    public Map<String,Object> grapRedPacket(int redPacketId ,int userId) {
+        int result = userRedPacketService.grapRedPacket(redPacketId, userId);
+        Map<String, Object> resultMap = new HashMap<String, Object>();
+        boolean flag = result > 0;
+        resultMap.put("success", flag);
+        resultMap.put("message", flag ? "抢红包成功" : "抢红包失败");
+        return resultMap;
+    }
+}
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,5 +1,5 @@
 <html>
 <body>
-<h2>Hello World!</h2>
+<h2>高并发抢红包业务</h2>
 </body>
 </html>
